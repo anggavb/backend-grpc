1256b6c8642b7f46fbcea5bf88099e93f4725d89
anggavb <anggavb8@gmail.com>
2026-06-20 17:01:23 +0700

    feat(tests): add tests for entry and transfer creation

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/backend-grpc/util"
+	"github.com/go-openapi/testify/v2/require"
+)
+
+func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
+	arg := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        util.RandomMoney(),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	createRandomTransfer(t, account1, account2)
+}
