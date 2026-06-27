8932ed27166cac97c9255ea5c385d719577b8fad
anggavb <anggavb8@gmail.com>
2026-06-27 14:07:56 +0700

    feat(token): implement JWT token creation and verification functionality

---
+++ token/jwt_maker_test.go
@@ -0,0 +1,67 @@
+package token
+
+import (
+	"log"
+	"testing"
+	"time"
+
+	"github.com/anggavb/simplebank/util"
+	"github.com/go-openapi/testify/v2/require"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func TestJWTMaker(t *testing.T) {
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt.Time, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiresAt.Time, time.Second)
+}
+
+func TestExpiredJWTToken(t *testing.T) {
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken(util.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	log.Println(err)
+	require.Error(t, err)
+	require.EqualError(t, err, jwt.ErrTokenExpired.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidJWTTokenAlgNone(t *testing.T) {
+	payload, err := NewPayload(util.RandomOwner(), time.Minute)
+	require.NoError(t, err)
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+	token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
+
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, jwt.ErrTokenSignatureInvalid.Error())
+	require.Nil(t, payload)
+}
